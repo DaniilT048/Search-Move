3f84407c74fa5c882bf10fece05ec8a269003daf
DaniilT048 <tis.daniil98@gmail.com>
2025-03-14 13:59:19 +0200

    make link

--- main.js
+++ main.js
@@ -2,5 +2,24 @@ const API_KEY = '2dc79949';
 const BASE_URL = 'https://www.omdbapi.com/';
 
 const inputMoveElement = document.querySelector('#searchMoveInput');
+const moveContainerElement = document.querySelector('#moveContainer');
 
-inputMoveElement.addEventListener('input', getMove)
+inputMoveElement.addEventListener('input', onSearchMove);
+
+async function onSearchMove(searchKey) {
+
+
+   const movieData = await fetch(getLink)
+        .then(res => res.json())
+        .then(response => {
+            if (response.Response === 'False') {
+                throw Error(res.Error);
+            }
+            console.log(getLink);
+            return response;
+        })
+    return movieData?.Search;
+}
+let searchKey = 'Harry'
+const getLink = `${BASE_URL}?apikey=${API_KEY}&s=${searchKey}`;
+console.log(getLink)
