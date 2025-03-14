c1b066b7d0476b17d91199e019e7f3560d7a7aa5
DaniilT048 <tis.daniil98@gmail.com>
2025-03-14 14:05:19 +0200

    make function for input text

--- main.js
+++ main.js
@@ -3,23 +3,30 @@ const BASE_URL = 'https://www.omdbapi.com/';
 
 const inputMoveElement = document.querySelector('#searchMoveInput');
 const moveContainerElement = document.querySelector('#moveContainer');
+const errorContainerElement = document.querySelector('#errorContainer');
 
 inputMoveElement.addEventListener('input', onSearchMove);
 
-async function onSearchMove(searchKey) {
+async function onInputText(){
+    moveContainerElement.innerHTML = ''
+    errorContainerElement.innerHTML = ''
 
+
+}
+
+
+
+async function onSearchMove(searchKey) {
+    const getLink = `${BASE_URL}?apikey=${API_KEY}&s=${searchKey}`;
 
    const movieData = await fetch(getLink)
         .then(res => res.json())
         .then(response => {
             if (response.Response === 'False') {
-                throw Error(res.Error);
+                throw Error();
             }
             console.log(getLink);
             return response;
         })
     return movieData?.Search;
 }
-let searchKey = 'Harry'
-const getLink = `${BASE_URL}?apikey=${API_KEY}&s=${searchKey}`;
-console.log(getLink)
